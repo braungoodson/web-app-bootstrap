6f48e2df6059e6217d1f074e4f11f1f82fb6d367
braun <braun@brauns-iMac.local>
2013-11-14 21:12:48 -0500

    + OK

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-var fs = require('fs');
-var cache = {
-  add: function (file) {
-    fs.readFile(file,function(e,d){
-      if (e) {
-        console.log('Error: Could not read file: %s',file);
-      } else {
-        cache.files[file] = d;
-        console.log('Cache: Added %s!',file);
-      }
-    });
-  },
-  files: []
-}
-cache.add('app/index.html');
-cache.add('app/css/starter-template.css');
+/**/
+
+//
+var fileCache = [];
+fileCache.push('app/index.html');
+fileCache.push('app/css/starter-template.css');
+
+//
 var mario = require('mario-mario');
 mario.plumbing({
   port: process.env.PORT || 10000,
+  fileCache: [
+    'app/index.html',
+    'app/css/starter-template.css'
+  ],
   http: {
     get: {
-      '/': function (q,r) {
-        return r.setHeader('Content-Type','text/html') + r.send(cache.files['app/index.html']);
+      '/': function (q,r,c) {
+        return r.setHeader('Content-Type','text/html') + r.send(c.files[fileCache[0]]);
       },
-      '/css/starter-template.css': function (q,r) {
-        return r.setHeader('Content-Type','text/css') + r.send(cache.files['app/css/starter-template.css']);
+      '/css/starter-template.css': function (q,r,c) {
+        return r.setHeader('Content-Type','text/css') + r.send(c.files[fileCache[1]]);
       }
     }
   }
