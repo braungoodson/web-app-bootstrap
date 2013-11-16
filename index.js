d72d053b851ebcc8a59c6943c7e0abf228f86fc0
braun <braun@brauns-iMac.local>
2013-11-15 22:26:16 -0500

    + login.css route

--- index.js
+++ index.js
@@ -4,15 +4,13 @@
 var fileCache = [];
 fileCache.push('app/index.html');
 fileCache.push('app/css/starter-template.css');
+fileCache.push('app/css/login.css');
 
 //
 var mario = require('mario-mario');
 mario.plumbing({
   port: process.env.PORT || 10000,
-  fileCache: [
-    'app/index.html',
-    'app/css/starter-template.css'
-  ],
+  fileCache: fileCache,
   http: {
     get: {
       '/': function (q,r,c) {
@@ -20,6 +18,9 @@ mario.plumbing({
       },
       '/css/starter-template.css': function (q,r,c) {
         return r.setHeader('Content-Type','text/css') + r.send(c.files[fileCache[1]]);
+      },
+      '/css/login.css': function (q,r,c) {
+        return r.setHeader('Content-Type','text/css') + r.send(c.file[fileCache[2]]);
       }
     }
   }
