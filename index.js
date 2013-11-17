1e3f3c3e5eb4d128cf61304a970ea1ad3e887f7e
braun <braun@brauns-iMac.local>
2013-11-16 21:41:09 -0500

    + links

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
 //
 var fileCache = [];
 fileCache.push('app/index.html');
+fileCache.push('app/css/signin.css');
 fileCache.push('app/css/starter-template.css');
-fileCache.push('app/css/login.css');
-fileCache.push('app/login.html');
 
 //
 var mario = require('mario-mario');
@@ -18,13 +17,10 @@ mario.plumbing({
         return r.setHeader('Content-Type','text/html') + r.send(c.files[fileCache[0]]);
       },
       '/css/starter-template.css': function (q,r,c) {
-        return r.setHeader('Content-Type','text/css') + r.send(c.files[fileCache[1]]);
+        return r.setHeader('Content-Type','text/css') + r.send(c.files[fileCache[2]]);
       },
-      '/css/login.css': function (q,r,c) {
-        return r.setHeader('Content-Type','text/css') + r.send(c.file[fileCache[2]]);
-      },
-      '/login': function (q,r,c) {
-        return r.setHeader('Content-Type','text/html') + r.send(c.file[fileCache[3]]);
+      '/css/signin.css': function (q,r,c) {
+        return r.setHeader('Content-Type','text/css') + r.send(c.file[fileCache[1]]);
       }
     }
   }
