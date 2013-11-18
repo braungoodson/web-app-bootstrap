39c6568ec63920a562208445815218c06b80d802
braun <braun@brauns-iMac.local>
2013-11-17 21:13:39 -0500

    + OK

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ fileCache.push('app/index.html');
 fileCache.push('app/css/signin.css');
 fileCache.push('app/css/starter-template.css');
 fileCache.push('app/libraries/md5-min.js');
-fileCache.push('app/signin.html');
+
 //
 var mario = require('mario-mario');
 mario.plumbing({
@@ -30,10 +30,6 @@ mario.plumbing({
         return r.setHeader('Content-Type','text/css') 
         + r.send(files[fileCache[3]]);
       },
-      '/signin': function (q,r,files,users) {
-        return r.setHeader('Content-Type','text/html') 
-        + r.send(files[fileCache[4]]);
-      },
       '/signin/:hash': function (q,r,files,users) {
         // if user exists
         // if user does not exist
@@ -47,19 +43,6 @@ mario.plumbing({
         + r.send({name:'braungoodson@gmail.com'});
       }
     },
-    post: {
-      '/signin': function (q,r,files,users) {
-        // if user exists
-        // if user does not exist
-        // if password is correct
-        // if password is incorrect
-        /*
-
-        */
-        console.log(q.params);
-        return r.setHeader('Content-Type','application/json') 
-        + r.send({name:'braungoodson@gmail.com'});
-      }
-    }
+    post: {}
   }
 });
