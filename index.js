e104bd517803137f938fa4b7aca757177fa69979
braun <braun@brauns-iMac.local>
2013-11-19 19:13:46 -0500

    - files,users; + fileNames[] and files[]: + mario-mario@0.0.7-lite;

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 /**/
 
-//
-var fileCache = [];
-fileCache.push('app/index.html');
-fileCache.push('app/css/signin.css');
-fileCache.push('app/css/starter-template.css');
-fileCache.push('app/libraries/sha1-min.js');
+var fs = require('fs');
+var filesNames = ['app/index.html'
+,'app/css/signin.css'
+,'app/css/starter-template.css'
+,'app/libraries/sha1-min.js'
+];
+for (var i in fileNames) {
+fs.readFile(filesNames[i],function(e,d){
+	if (e) {
+		console.log('Error: Could not read file: %s from:\n %s',e,fileNames[i]);
+	} else {
+		files[fileNames[i]] = d;
+		console.log('Cached: %s",fileNames[i]);
+	}
+});
+}
 
 //
 var hex_hmac_sha1 = require('sha1-min-0.0.1');
@@ -13,36 +23,34 @@ var hex_hmac_sha1 = require('sha1-min-0.0.1');
 //
 var mario = require('mario-mario');
 mario.plumbing({
-  port: process.env.PORT || 10000,
-  fileCache: fileCache,
   http: {
     get: {
-      '/': function (q,r,files,users) {
+      '/': function (q,r) {
         return r.setHeader('Content-Type','text/html') 
-        + r.send(files[fileCache[0]]);
+        + r.send(files[0]);
       },
-      '/css/starter-template.css': function (q,r,files,users) {
+      '/css/starter-template.css': function (q,r) {
         return r.setHeader('Content-Type','text/css') 
-        + r.send(files[fileCache[2]]);
+        + r.send(files[2]);
       },
-      '/css/signin.css': function (q,r,files,users) {
+      '/css/signin.css': function (q,r) {
         return r.setHeader('Content-Type','text/css') 
-        + r.send(files[fileCache[1]]);
+        + r.send(files[1]);
       },
-      '/libraries/sha1-min.js': function (q,r,files,users) {
+      '/libraries/sha1-min.js': function (q,r) {
         return r.setHeader('Content-Type','text/css') 
-        + r.send(files[fileCache[3]]);
+        + r.send(files[3]);
       },
-      '/signin': function (q,r,files,users) {
+      '/signin': function (q,r) {
         var challenge = new Date().getTime();
         var salt = 'This is the salt.';
         q.session.challenge = challenge;
-        console.log("New Challenge!");
+        console.log("Challenge: %s",challenge);
         return r.setHeader('X-Challenge',challenge) 
         + r.setHeader('X-Salt',salt)
-        + r.send('');
+        + r.send({challenge:challenge,salt:salt});
       },
-      '/signin/name/:name/hash/:hash': function (q,r,files,users) {
+      '/signin/name/:name/hash/:hash': function (q,r) {
         var signin = false;
         var hash = q.params.hash;
         var challenge = q.session.challenge;
@@ -59,7 +67,6 @@ mario.plumbing({
         return r.setHeader('Content-Type','application/json') 
         + r.send({signin:signin});
       }
-    },
-    post: {}
+    }
   }
 });
