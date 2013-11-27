829d6e22532b93d7ec899587eb90d5f925f292fe
braun <braun@brauns-iMac.local>
2013-11-27 18:06:34 -0500

    + signup; + bug;

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 //
 var fs = require('fs');
-var fileNames = ['app/index.html'
-,'app/css/starter-template.css'
-,'app/css/signin.css'
+var fileNames = [
+  'app/index.html'
+  ,'app/css/starter-template.css'
+  ,'app/css/signin.css'
 ];
 var files = [];
 for (var i in fileNames) {
@@ -20,8 +21,21 @@ for (var i in fileNames) {
 }
 
 //
+function User (o) {
+  this.name = o.name;
+  this.password = o.password;
+  this.data = o.data;
+}
+User.prototype.getUser = function () {
+  return {
+    name: this.name,
+    data: this.data
+  }
+}
 var users = [];
-users['braun'] = {name:'braun',password:'braun',data:null,getUser:function(){return{name:this.name,data:this.data}}};
+users['braun'] = new User({name:'braun',password:'braun',data:null});
+users['braun1'] = new User({name:'braun1',password:'braun1',data:null});
+//,getUser:function(){return{name:this.name,data:this.data}}};
 
 //
 var mario = require('mario-mario');
@@ -42,6 +56,14 @@ mario.plumbing({
       },
       '/signin/:name/:password': function (q,r) {
         if (users[q.params.name]) {
+          if (q.session.attempts) {
+            q.session.attempts++;
+            if (q.session.attempts > 3) {
+              return r.send(403);
+            }
+          } else {
+            q.session.attempts = 1;
+          }
           if (users[q.params.name].password == q.params.password) {
             var user = users[q.params.name].getUser();
             q.session.signedin = true;
@@ -50,7 +72,29 @@ mario.plumbing({
             + r.send(200,{user:user});
           }
         }
-        return r.send(403);
+        return r.send(401);
+      },
+      '/signup/:name/:password': function (q,r) {
+        console.log(q.session.attempts)
+        if (q.session.attempts) {
+          q.session.attempts++;
+          if (q.session.attempts > 3) {
+            return r.send(403);
+          }
+        } else {
+          q.session.attempts = 1;
+        }
+        if (!users[q.params.name]) {
+          var u = q.params;
+          var user = new User({name:u.name,password:u.password,data:null});
+          users[user.name] = user;
+          q.session.signedin = true;
+          q.session.user = user;
+          return r.setHeader('Content-Type','application/json')
+          + r.send(200,{user:user.getUser()});
+        } else {
+          return r.send(400);
+        }
       }
     }
   }
